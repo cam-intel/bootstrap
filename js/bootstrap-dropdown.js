30f6dc9feb61611400becd8a4bd700d9e10844c5
Joe Parry <joe@cam-intel.com>
2011-10-07 17:38:59 +0100

    lack of semi-colons caused bmi.js insertion script to bork it

--- js/bootstrap-dropdown.js
+++ js/bootstrap-dropdown.js
@@ -20,16 +20,16 @@
 
 (function( $ ){
 
-  var d = 'a.menu, .dropdown-toggle'
+  var d = 'a.menu, .dropdown-toggle';
 
   function clearMenus() {
-    $(d).parent('li').removeClass('open')
+    $(d).parent('li').removeClass('open');
   }
 
   $(function () {
-    $('html').bind("click", clearMenus)
-    $('body').dropdown( '[data-dropdown] a.menu, [data-dropdown] .dropdown-toggle' )
-  })
+    $('html').bind("click", clearMenus);
+    $('body').dropdown( '[data-dropdown] a.menu, [data-dropdown] .dropdown-toggle' );
+  });
 
   /* DROPDOWN PLUGIN DEFINITION
    * ========================== */
@@ -38,11 +38,11 @@
     return this.each(function () {
       $(this).delegate(selector || d, 'click', function (e) {
         var li = $(this).parent('li')
-          , isActive = li.hasClass('open')
+          , isActive = li.hasClass('open');
 
-        clearMenus()
-        !isActive && li.toggleClass('open')
-        return false
+        clearMenus();
+        !isActive && li.toggleClass('open');
+        return false;
       })
     })
   }
